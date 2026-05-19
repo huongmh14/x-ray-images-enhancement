9e289ace1e7afc0be3e52fc034bbc2246b1c8975
Vector 52hz <huonghoang789hp@gmail.com>
2026-05-19 14:06:34 +0700

    update 02

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from src.algorithms.runner import AlgorithmRunner
+# This is the main entry point of the application. It initializes the AlgorithmRunner and starts the execution of the algorithms.	
 
 def main():
 	ar = AlgorithmRunner()
